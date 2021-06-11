1178463383d2856c0b6734aec40db9bb38a786a3
kunglaw <alhusna901@gmail.com>
2021-06-11 22:19:06 +0700

    Updates

---
+++ precache-manifest.3004dba4e6c80501656bc54181885755.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a68eca5072c2ade118f8931c4a7fa2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd9eacbf59da78320fa6",
+    "url": "/static/css/2.b0704b7a.chunk.css"
+  },
+  {
+    "revision": "48a0562db41413e9e4e9",
+    "url": "/static/css/main.5c08abe8.chunk.css"
+  },
+  {
+    "revision": "cd9eacbf59da78320fa6",
+    "url": "/static/js/2.4263f901.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.4263f901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48a0562db41413e9e4e9",
+    "url": "/static/js/main.ede5d856.chunk.js"
+  },
+  {
+    "revision": "f5d0a8c0d53d7926480c",
+    "url": "/static/js/runtime-main.37b7fcf9.js"
+  }
+]);
